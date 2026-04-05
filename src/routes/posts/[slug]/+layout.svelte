66c9248d4219577d114ec3945c9ffdc03302e32e
Andreas Lauhard <lauhard@proton.me>
2026-04-05 19:30:18 +0200

    chore: updated layout - simplified post layout

--- src/routes/posts/[slug]/+layout.svelte
+++ src/routes/posts/[slug]/+layout.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
     import NavigationItem from "$lib/components/NavigationItem.svelte";
     import type { Route } from "$lib/routes";
-    import {
-        LucideArrowLeftFromLine,
-        LucideArrowRightFromLine,
-    } from "lucide-svelte";
+    import { LucideChevronFirst, LucideChevronLast } from "lucide-svelte";
     import type { Snippet } from "svelte";
     let {
         children,
@@ -17,7 +14,6 @@
         };
     } = $props();
 
-    console.log("data", data)
     let prevRoute = $derived<Route>({
         path: data.prevPost?.slug ?? "",
         name: data.prevPost?.title ?? "",
@@ -30,18 +26,17 @@
         prevRoute = {
             path: "/posts/" + (data.prevPost?.slug ?? ""),
             name: data.prevPost?.title ?? "",
-            icon: data.prevPost?.title ? LucideArrowLeftFromLine : undefined,
+            icon: data.prevPost?.title ? LucideChevronFirst : undefined,
         };
         nextRoute = {
             path: "/posts/" + (data.nextPost?.slug ?? ""),
             name: data.nextPost?.title ?? "",
-            icon: data.nextPost?.title ? LucideArrowRightFromLine : undefined,
+            icon: data.nextPost?.title ? LucideChevronLast : undefined,
         };
     });
 </script>
 
 <div class="wrapper">
-    
     <div>
         {@render children()}
     </div>
@@ -60,7 +55,6 @@
     .wrapper {
         display: flex;
         flex-direction: column;
-        
     }
     menu {
         margin: 0;
@@ -70,28 +64,33 @@
         display: flex;
         justify-content: space-between;
         align-items: center;
-        min-height: 80px;
-        height: auto;
+        height: fit-content;
         background-color: var(--color-ld-white-400);
         margin-top: 2rem;
-        margin-bottom:.5rem;
+        margin-bottom: 0.5rem;
         border-radius: 1rem;
+        text-wrap: balance;
+        gap: 0.5rem;
     }
     :global(.nav-item) {
         width: 50%;
         display: flex;
         flex: 1;
-        
+        height: 100%;
+        padding-block: 0.2rem;
+        border-radius: 5px;
+        border: 1px solid var(--color-ld-accent-600);
+
     }
     :global(.nav-link) {
         width: 50%;
         display: flex;
         max-height: 80px;
+        height: max-content;
         flex: 1;
         justify-content: flex-start;
         font-weight: normal;
-        line-height: 1.6rem;
-        
+        line-height: 1.5rem;
     }
     :global(.nav-item-right > a) {
         display: flex;
@@ -101,31 +100,36 @@
     :global(.nav-item > a:has(svg) > svg) {
         min-width: 45px;
         min-height: 45px;
-        padding: 0.6rem;
-        margin-inline: 1rem;
+        padding: 0.3rem;
+        margin-inline: 0rem;
         border-radius: 100%;
+        stroke-width: 0.1rem;
         color: var(--color-ld-black-500);
-        border: 1px var(--color-ld-accent-600) solid;
     }
 
     :global(.nav-item:hover a > svg) {
         color: var(--color-ld-accent-700);
     }
-   
+
+ 
+  
+
     @media (width < 800px) {
+        menu {
+            flex-direction: column;
+        }
+        :global(.nav-item) {
+            width: 100%;
+        }
         :global(.nav-link) {
-            text-align: normal;
-        }
-        :global(.nav-item-right) {
-            justify-content: end;
-        }
-    }
-    @media (width < 600px) {
-        :global(.nav-link) {
-            justify-content: space-between;
-        }
-        :global(.link-text) {
-            display: none;
+            width: 50%;
+            display: flex;
+            max-height: 80px;
+            height: auto;
+            flex: 1;
+            justify-content: flex-start;
+            font-weight: normal;
+            line-height: 1.5rem;
         }
     }
 </style>
